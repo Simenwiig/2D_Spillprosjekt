36a961fe03fec57319a5bfe5ec889ce4ee2e970e
Zincol <73915697+Zincol@users.noreply.github.com>
2021-03-10 13:59:59 +0100

    Add files via upload
    Første gang jeg har gjort dette, håper det funker

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    [Header("Settings")]
+
+    public float moveSpeed = 2f;
+    public float sprintSpeedModifier = 2f;
+
+    [Header("Attributes")]
+
+    public float healthLevel;
+    public float hungerLevel;
+    public float thirstLevel;
+    public float smellLevel;
+    public float speedLevel { get { return velocity.magnitude; } }
+    public Vector3 velocity;
+
+    float temp_Guncooldown;
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        transform.tag = "Player";
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Walk();
+
+        Aim();
+
+        Resources();
+
+
+        transform.position += velocity * Time.fixedDeltaTime;
+    }
+
+    void Walk()
+    {
+        int vertical = (Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0);
+        int horizontal = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
+
+        velocity = (Vector2.up * vertical + Vector2.right * horizontal).normalized * moveSpeed;
+    }
+
+  
+
+    void Aim()
+    {
+        int vertical = (Input.GetKey(KeyCode.UpArrow) ? 1 : 0) + (Input.GetKey(KeyCode.DownArrow) ? -1 : 0);
+        int horizontal = (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) + (Input.GetKey(KeyCode.LeftArrow) ? -1 : 0);
+
+  
+
+        Vector2 aimDirection = (Vector2.up * vertical + Vector2.right * horizontal).normalized;
+
+        bool pullTrigger = aimDirection.magnitude > 0;
+
+        temp_Guncooldown -= Time.deltaTime;
+
+        if (pullTrigger && temp_Guncooldown < 0)
+        {
+            temp_Guncooldown = 0.4f;
+
+            Debug.DrawRay(transform.position - Vector3.forward, aimDirection, Color.yellow, 0.1f);
+            
+        }
+
+    }
+
+    void Resources()
+    {
+        bool isWalking = speedLevel > 0;
+        bool isRunning = speedLevel > moveSpeed;
+
+        
+
+    }
+
+}
